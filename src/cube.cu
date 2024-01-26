418b73c76b6b80c4f437f40a1b1203b0adff42a6
Guilherme A. <47290457+gcalbertini@users.noreply.github.com>
2024-01-26 05:16:55 -0500

    Update with bugs

--- src/cube.cu
+++ src/cube.cu
@@ -2,39 +2,32 @@
 #include <iostream>
 #include <math.h>
 
-__global__ void square(float * d_out, float * d_in)
-{
+__global__ void cube(float * d_out, float * d_in){
 	int idx = threadIdx.x;
 	float f = d_in[idx];
-	d_out[idx] = f * f;
+	d_out[idx] = f * f * f;
 }
 
-int main(int argc, char** argv)
-{
-	constexpr int ARRAY_SIZE {64};
-	constexpr int ARRAY_BYTES {ARRAY_SIZE * sizeof(float)};
+int main(int argc, char ** argv){
+	constexpr int ARRAY_SIZE {96};
+	constexpr int ARRAY_BYTES {ARRAY_SIZE*sizeof(float)}; 
 
-	// generate input array on host
-	float h_in[ARRAY_SIZE] {}; //  float* h_in = new float[ARRAY_SIZE]{}; in C++ --> std::unique_ptr<float[]> h_in = std::make_unique<float[]>(ARRAY_SIZE);
+	float h_in[ARRAY_SIZE];
+
 	for (int i = 0; i < ARRAY_SIZE; ++i){
 		h_in[i] = float(i);
 	}
-	float h_out[ARRAY_SIZE] {};
+	float h_out[ARRAY_SIZE];
 
-	// gpu mem ptrs
-	float * d_out, * d_in;
+	float * d_in, * d_out;
 
-	//allocate GPU mem
 	cudaMalloc((void**) &d_in, ARRAY_BYTES);
 	cudaMalloc((void**) &d_out, ARRAY_BYTES);
 
-	//transfer array to GPU
 	cudaMemcpy(d_in, h_in, ARRAY_SIZE, cudaMemcpyHostToDevice);
 
-	//launch Kernel 
-	square<<<1, ARRAY_SIZE>>>(d_out, d_in);
+	cube<<<1, ARRAY_SIZE>>>(d_out, d_in);
 
-	//transfer from GPU to CPU
 	cudaMemcpy(h_out, d_out, ARRAY_SIZE, cudaMemcpyDeviceToHost);
 
 	// print out resulting array
@@ -47,5 +40,6 @@ int main(int argc, char** argv)
 	cudaFree(d_out);
 
 	return 0;
+
 
 }
